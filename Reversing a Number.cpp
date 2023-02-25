9c81c14fa43e048f7966765ea5c067002b915b0d
suhas0407 <118986814+suhas0407@users.noreply.github.com>
2023-02-26 00:42:27 +0530

    Add files via upload

---
+++ Reversing a Number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int r,n,rev=0;
+
+    cout<<"Enter n";
+    cin>>n;
+
+    while(n!=0)
+    {
+        r=n%10;
+        n=n/10;
+        rev=rev*10+r;
+    }
+    cout<<"Reverse number is"<<rev;
+    return 0;
+}
